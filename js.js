981a8eb3a41ac0f58dda4e581f599ce70867a885
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2024-02-22 16:45:23 -0300

    Perguntas

--- js.js
+++ js.js
@@ -1,8 +1,60 @@
 const perguntas = [
     {
-        pergunta: "Pergunta 01", respostas: ["Resposta A", "Resposta B", "Resposta C"],
+        pergunta: "Como você declara uma variável em JavaScript?",
+        respostas: ["let myVar;", "var myVar;", "const myVar;"],
+        correta: 1
+    },
+    {
+        pergunta: "Qual é a função utilizada para imprimir algo no console em JavaScript?",
+        respostas: ["print();", "log();", "console.log();"],
         correta: 2
     },
+    {
+        pergunta: "Qual é a forma correta de declarar uma função em JavaScript?",
+        respostas: ["function myFunction() {};", "myFunction = function() {};", "let myFunction = () => {};"],
+        correta: 0
+    },
+    {
+        pergunta: "Qual método é usado para adicionar um elemento ao final de um array em JavaScript?",
+        respostas: ["push()", "add()", "append()"],
+        correta: 0
+    },
+    {
+        pergunta: "Qual é o operador de igualdade estrita em JavaScript?",
+        respostas: ["==", "===", "!="],
+        correta: 1
+    },
+    {
+        pergunta: "Como você verifica se um valor é do tipo array em JavaScript?",
+        respostas: ["isArray(myArray);", "myArray.isArray();", "Array.isArray(myArray);"],
+        correta: 2
+    },
+    {
+        pergunta: "Qual é a palavra-chave usada para criar um loop em JavaScript?",
+        respostas: ["for", "loop", "repeat"],
+        correta: 0
+    },
+    {
+        pergunta: "Qual é a forma correta de comentar várias linhas em JavaScript?",
+        respostas: ["// This is a comment", "<!-- This is a comment -->", "/* This is a comment */"],
+        correta: 2
+    },
+    {
+        pergunta: "Qual é o método usado para remover o último elemento de um array em JavaScript?",
+        respostas: ["pop()", "remove()", "deleteLast()"],
+        correta: 0
+    },
+    {
+        pergunta: "Como você declara uma constante em JavaScript?",
+        respostas: ["let", "const", "var"],
+        correta: 1
+    }
+];
 
-]
-alert(perguntas[o].pergunta)
+const template = document.querySelector('template');
+const quizItem = template.content.cloneNode(true)
+
+for (let item of perguntas) {
+    alert(item.pergunta)
+
+}
